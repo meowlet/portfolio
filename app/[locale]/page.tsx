93bc2afcf34bb27e45cd6b313c1b972bc53bffa7
meowlet <ngoclink13@gmail.com>
2024-12-07 06:59:31 +0700

    chore: update project dependencies
    - Update TypeScript to 5.7.2
    - Update React types
    - Update development dependencies
    - Update PostCSS and TailwindCSS

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "../components/home/Hero";
+import About from "../components/home/About";
+import Projects from "../components/home/Projects";
+import Skills from "../components/home/Skills";
+import Contact from "../components/home/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
+  );
+}
