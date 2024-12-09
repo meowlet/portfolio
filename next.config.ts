b78b743fc65a200d4a88bf9b7667b6d19ad6c039
meowlet <ngoclink13@gmail.com>
2024-12-09 23:15:09 +0700

    chore: update Next.js configuration to allow image domains
    - Added image domains for Unsplash and Picsum to the Next.js configuration.
    - Maintained existing TypeScript settings to ignore build errors.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const config = {
-  // Skip type checking and linting during production build because we already have them in the CI, and it's can cause the server to crash
+  images: {
+    domains: ["images.unsplash.com", "picsum.photos"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
