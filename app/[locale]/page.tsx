76ba874f14a025198bafbf96296f8637013a3a67
meowlet <ngoclink13@gmail.com>
2024-12-09 22:59:18 +0700

    chore: update component imports and add CoverDesign component
       - Updated import paths for home components to use absolute paths.
       - Added CoverDesign component to the home page.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,16 @@
-import Hero from "../components/home/Hero";
-import About from "../components/home/About";
-import Projects from "../components/home/Projects";
-import Skills from "../components/home/Skills";
-import Contact from "../components/home/Contact";
+import Hero from "@/app/components/home/Hero";
+import About from "@/app/components/home/About";
+import Projects from "@/app/components/home/Projects";
+import Skills from "@/app/components/home/Skills";
+import Contact from "@/app/components/home/Contact";
+import CoverDesign from "@/app/components/home/CoverDesign";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <Hero />
       <About />
+      <CoverDesign />
       <Projects />
       <Skills />
       <Contact />
